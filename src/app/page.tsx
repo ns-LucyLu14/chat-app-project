069981e68208f5715e16cc2866d51ee0027be665
Domagoj <d.lucic14@gmail.com>
2024-10-06 23:31:16 +0200

    code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,33 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
-
-import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { api } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
-  void api.post.getLatest.prefetch();
-
-  console.log("session", session?.user);
-
   if (session) redirect("/dashboard");
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Domagoj <span className="text-[hsl(280,100%,70%)]">Chat-App</span>{" "}
-            Project
-          </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Domagoj <span className="text-[hsl(280,100%,70%)]">Chat-App</span>{" "}
+          Project
+        </h1>
 
-          <div className="flex flex-col items-center gap-2">
-            <div className="flex flex-col items-center justify-center gap-4">
-              <Link
-                href={"/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                Sign in
-              </Link>
-            </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Link
+              href={"/api/auth/signin"}
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              Sign in
+            </Link>
           </div>
         </div>
-      </main>
-    </HydrateClient>
+      </div>
+    </main>
   );
 }
