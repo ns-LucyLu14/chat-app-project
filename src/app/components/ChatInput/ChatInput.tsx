6e90ebd8f5b069b28142cf01aae65339463c37f0
Domagoj <d.lucic14@gmail.com>
2024-10-07 17:19:36 +0200

    add smile and wink emoji functionality

--- src/app/components/ChatInput/ChatInput.tsx
+++ src/app/components/ChatInput/ChatInput.tsx
@@ -7,10 +7,22 @@ type ChatInputProps = {
   chatPartner: string;
 };
 
+const replaceIcons = {
+  smiley: "😊",
+  wink: "😉",
+};
+
 const ChatInput = ({ chatPartner }: ChatInputProps) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [input, setInput] = useState<string>("");
   const sendMessage = () => {};
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let value = e.target.value;
+    value = value.replace(/(:\))/g, replaceIcons.smiley);
+    value = value.replace(/(;\))/g, replaceIcons.wink);
+    setInput(value);
+  };
   return (
     <div className="border-t border-gray-200 p-4 px-2 sm:mb-0">
       <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
@@ -24,7 +36,7 @@ const ChatInput = ({ chatPartner }: ChatInputProps) => {
           }}
           rows={1}
           value={input}
-          onChange={(e) => setInput(e.target.value)}
+          onChange={handleChange}
           placeholder={`Send a message to ${chatPartner}...`}
           className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:p-1.5 sm:text-sm sm:leading-6"
         />
